a34e066c7aea6eea54cd86da5934d2d9ec6c3cc0
syedr <email_address>
2024-10-15 00:35:02 -0400

    update server.js

--- weather-app-backend/server.js
+++ weather-app-backend/server.js
@@ -10,9 +10,7 @@ const port = process.env.PORT || 3000;
 dotenv.config();
 
 // Use CORS
-app.use(cors({
-    origin: 'https://weatherapp-production-acf5.up.railway.app/', 
-}));
+app.use(cors());
 
 // Root route: Simple endpoint to confirm the API is running
 app.get('/', (req, res) => {
